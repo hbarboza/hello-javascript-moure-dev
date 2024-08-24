fe7bfe772e1bb323a864d5f0c165e1a22d63093d
Hugo Barboza <hugo.barboza@infocorpgroup.com>
2024-08-24 17:39:23 -0300

    Resolucion de ejercicios iniciales

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -5,12 +5,28 @@ https://www.youtube.com/live/euVbF1eatEY?si=nHS3QznAlA_C39NK&t=808
 */
 
 // 1. Escribe un comentario en una línea
+// Esto es un comentario en una línea
 
 // 2. Escribe un comentario en varias líneas
+/*Esto es un comentario
+de varias lineas
+*/
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+let userName = "MoureDev";
+let age = 30;
+let isDeveloper = true;
+let nullValue = null;
+let undefinedValue;
+let mySymbol = Symbol("mysymbol");
+let myBigInt = BigInt(817239871289371986589716389471628379612983761289376129);
+let myBigInt2 = 817239871289371986589716389471628379612983761289376129n;
 
 // 4. Imprime por consola el valor de todas las variables
+console.log(userName);
+
+
+
 
 // 5. Imprime por consola el tipo de todas las variables
 
